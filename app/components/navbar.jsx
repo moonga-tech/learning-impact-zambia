5b41b033074861458a190b4ae356fb0483b20007
moonga-tech <manongomoonga@gmail.com>
2025-07-09 21:13:08 +0200

    commit

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -20,7 +20,7 @@ export default function Nav() {
                         <h5 className="logo">Learning Impact Zambia (LIZ)</h5>
                     </Link>
                 </div>
-                
+
                 <div className="block lg:hidden">
                     <button className="flex items-center px-3 py-2 border rounded text-gray-800 border-gray-800 hover:text-gray-600 hover:border-white" id="toggleButton">
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
@@ -30,14 +30,14 @@ export default function Nav() {
                 <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto nav-links" id="nav-links">
                     <div className="text-sm lg:flex-grow">
                         <ul>
-                            <Link href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600 mr-4">
-                            Docs
+                            <Link href={"/"} className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600 mr-4">
+                            Home
                             </Link>
-                            <Link href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600 mr-4">
-                                Examples
+                            <Link href={"#our-mission"} className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600 mr-4">
+                                Our Mission
                             </Link>
-                            <Link href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600">
-                                Blog
+                            <Link href={"#our-vision"} className="block mt-4 lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600">
+                                Our Vision
                             </Link>
                         </ul>
                     </div>
